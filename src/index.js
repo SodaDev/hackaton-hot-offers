2ef707104a35c9a3ac8cf7b36ba914a6b8abc07b
Sodkiewicz, Marcin <sodkiewiczm@ryanair.com>
2017-04-21 12:11:11 +0200

    Service worker init

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import App from './components/App';
 
 import configureStore from './store/configureStore';
 import "./index.css";
+import { registerWorker } from "./sw"
 
 const store = configureStore();
 
 ReactDOM.render(
     <App store={store}>
-        <div>Ryanair hackaton</div>
+        <div>Ryanair hackaton with manifest</div>
     </App>,
     document.getElementById('root')
 );
+
+registerWorker();
