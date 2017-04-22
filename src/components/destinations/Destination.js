b094ce5297ee1d17dc4eec11cd98ce0a58c32527
Sodkiewicz, Marcin <sodkiewiczm@ryanair.com>
2017-04-22 12:17:20 +0200

    Missing airport image

--- src/components/destinations/Destination.js
+++ src/components/destinations/Destination.js
@@ -7,10 +7,11 @@ export const Destination = (props) => {
     addToWatchedHandler = partial(props.addToWatched, props.airport);
   }
 
+  const defaultUrl = "/img/default-destination.png";
   const imgUrl = `https://www.ryanair.com/etc/designs/ryanair/frontend/resources/ui/illustrations/destinations/${props.airport.iataCode}.png`;
   return (
     <div className={"section_single_dest " + (props.airport.isWatched ? ' disabled' : '')} id="barcelona" onClick={addToWatchedHandler}>
-      <img src={imgUrl} className="section_single_dest__thumbnail"/>
+      <img src={imgUrl} className="section_single_dest__thumbnail" onError={(e)=>{e.target.src=defaultUrl}}/>
       <h4 className="section_single_dest__city">
         {props.airport.name}
       </h4>
